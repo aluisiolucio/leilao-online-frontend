828d66fb7ca32592a82560778c456916a22c8893
Aluisio Lucio <aluisio18lucio@gmail.com>
2024-05-10 16:42:21 -0300

    Correção na ordenação das listas de leilões

--- src/pages/highlightsAuctions.tsx
+++ src/pages/highlightsAuctions.tsx
@@ -44,7 +44,7 @@ export function HighlightsAuctions() {
                     <section>
                         <div className="flex items-start justify-between gap-4">
                             {
-                                auctions?.length || 0 > 0 ? auctions?.slice(0, 5).map((auction: Auction) => (
+                                auctions?.length || 0 > 0 ? auctions?.reverse().slice(0, 5).map((auction: Auction) => (
                                     <Link key={Math.random()} to={"/auction/details/" + auction.id}>
                                         <Card
                                             title={auction.title}
@@ -72,7 +72,7 @@ export function HighlightsAuctions() {
                     <section>
                         <div className="flex items-start justify-between gap-4">
                             {
-                                auctions?.length || 0 > 0 ? auctions?.reverse().slice(0, 8).map((auction: Auction) => (
+                                auctions?.length || 0 > 0 ? auctions?.slice(0, 8).map((auction: Auction) => (
                                     <Link key={Math.random()} to={"/auction/details/" + auction.id}>
                                         <Card
                                             title={auction.title}
