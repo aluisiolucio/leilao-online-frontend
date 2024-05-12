23f6eff80e2c7ba9991647e0e440b840d1cb78d1
Aluisio Lucio <aluisio18lucio@gmail.com>
2024-05-12 03:47:56 -0300

    Add polling

---
+++ src/components/batchsHTTP.tsx
@@ -0,0 +1,85 @@
+import { useRef, useEffect, useState } from 'react';
+import { formatPrice } from "@/lib/utils";
+import { useFetch } from '@/hooks/useFetch';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
+
+type JwtDecoded = {
+    id: string;
+}
+
+export function BatchsHTTP({ batchId }: { batchId: string }) {
+    const { data: initialLances, error } = useFetch<any>('/batch/' + batchId + '/lances');
+    const [lances, setLances] = useState(initialLances);
+
+    if (error) {
+        console.log(error);
+    }
+
+    const token = localStorage.getItem('accessToken');
+    const decoded = jwtDecode<JwtDecoded>(token || '');
+    const userId = decoded.id;
+
+    const endOfListRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            try {
+                const response = await axios.get('http://3.239.191.235:3000/api/batch/' + batchId + '/lances',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        }
+                    }
+                );
+
+                // if (!response.data) {
+                //     return () => {
+                //         clearInterval(interval);
+
+                //         window.location.reload();
+                //     };
+                // }
+
+                setLances(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar os dados:', error);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [batchId]); // A dependência batchId garante que o intervalo seja reiniciado quando batchId muda
+
+    useEffect(() => {
+        if (endOfListRef.current) {
+            endOfListRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [lances]);
+
+    return (
+        <div className="col-span-3 flex items-start gap-4">
+            <div className="w-full rounded-xl border bg-card text-card-foreground shadow p-5 space-y-6 overflow-auto max-h-96">
+                {
+                    lances && lances.length > 0 ? (
+                        lances.map((lance: any, index: any) => (
+                            <div key={index} className={`w-fit ${lance.userId === userId && "ml-auto"}`}>
+                                <div className={`text-sm font-medium ${lance.userId === userId && "text-right"}`}>{lance.userName} - {lance.time}</div>
+                                <div className={`w-fit mt-2 bg-primary py-2 px-4 rounded-lg ${lance.userId === userId && "ml-auto"}`}>
+                                    <p className="text-sm text-secondary">Lance n° {lance.code}</p>
+                                    <div className="mt-1 font-semibold text-slate-900">{formatPrice(Number(lance.value))}</div>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text-center text-sm text-slate-500">
+                            Nenhum lance registrado. Os lances registrados aparecerão aqui.
+                        </div>
+                    )
+                }
+                {/* Elemento invisível no fim da lista para o scroll */}
+                <div ref={endOfListRef}></div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/cardSendBatchHTTP.tsx
@@ -0,0 +1,60 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SendHorizontal } from "lucide-react";
+import { useState } from "react";
+import { useSend } from "@/hooks/useSend";
+
+type Lance = {
+    batchId: string;
+}
+
+export function CardSendBatchHTTP({ batchId }: Lance) {
+    const [batchValue, setBatchValue] = useState<string>('');
+
+    const { error, sendRequest } = useSend<any>(
+        "http://3.239.191.235:3000/api/batch/lances",
+        true
+    );
+
+    if (error) {
+        console.log(error);
+    }
+
+    const handleSendBatch = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        if (!batchValue) {
+            return;
+        }
+
+        await sendRequest({
+            batchId: batchId,
+            value: batchValue
+        })
+    };
+
+    return (
+        <div className="col-span-1 ml-3">
+            <div className="w-full rounded-xl border bg-card text-card-foreground shadow p-5">
+                <div className="space-y-2">
+                    <h2 className="text-xl font-medium">Faça seu lance!</h2>
+                    <p className="text-sm text-muted-foreground">Informe o valor no campo abaixo e clique em enviar para efetuar um lance.</p>
+                </div>
+                
+                <div className="mt-8 flex items-center gap-2">
+                    <Input
+                        id="value"
+                        type="text"
+                        placeholder="R$ 10.000,00"
+                        value={batchValue}
+                        onChange={(e) => setBatchValue(e.target.value)}
+                    />
+
+                    <Button variant={"outline"} onClick={handleSendBatch}>
+                        <SendHorizontal size={16} />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
